8472d8bd736df8f40f9a1a33e20a959977da6f75
theITGuy825 <e_henderson02@outlook.com>
2025-02-20 12:21:03 -0500

    aaa

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// Import Firebase SDK
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// Your Firebase config (get this from Firebase Console)
 const firebaseConfig = {
     apiKey: "AIzaSyAb7EcZ98mRD-VuGzyQZACBrcGPvR89iS0",
     authDomain: "webassingment-12663.firebaseapp.com",
@@ -13,9 +11,7 @@ const firebaseConfig = {
     appId: "1:253077413212:web:ab490f8c61a7547083b599"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Export db for use in other parts of your app
-export { db };
+export {db};
